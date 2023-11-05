37fd6171d7b6feb922bab015a69813e783ed26d8
s25y25n <144671761+s25y25n@users.noreply.github.com>
2023-11-05 11:44:20 +0900

    2-2(average)

--- main.c
+++ main.c
@@ -4,15 +4,18 @@
 
 int main(int argc, char *argv[])
 {
-    int i;
+    int i, average ;
     int grade[SIZE];
     printf("5명의 점수를 입력하세요 : \n");
     
-    for(i=0;i<SIZE;i++)
+    int sum = 0;
+    for(i=0;i<SIZE;i++) {
         scanf("%d", &grade[i]);
+        sum += grade[i];
+        }
         
-    for (i=0;i<SIZE;i++)
-        printf("grade[%d] = %d\n", i, grade[i]); 
+    average = sum / SIZE;
+    printf("점수 평균 : %d \n", average);
 
 
 
