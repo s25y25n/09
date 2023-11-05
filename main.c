097fc1e916438eaf231d058b73490fff04a2f213
s25y25n <144671761+s25y25n@users.noreply.github.com>
2023-11-05 11:36:48 +0900

    01

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N_STUDENT  5
+
+int main(int argc, char *argv[])
+{
+    int grade[N_STUDENT];
+    int i;
+    /*
+    grade[0] = 10 ;
+    grade[1] = 20 ;
+    grade[2] = 30 ;
+    grade[3] = 40 ;
+    grade[4] = 50 ;
+        */
+    for (i=0; i<N_STUDENT;i++) 
+        grade[i] = (i+1)*10 ;
+    
+    for(i=0;i<N_STUDENT;i++)
+        printf("grade[%i] = %i \n", i, grade[i]);
+    
+        
+  
+  system("PAUSE");	
+  return 0;
+}
